d7cbd2257deefb9cb3ba2dd7e25f3a3a8dd0f974
Arina Hristova <arinahri@ut.ee>
2024-10-24 00:17:30 +0300

    Добавьте файлы проекта.

---
+++ task 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int a = rand.Next(1, 100);
+            Console.WriteLine("Please, enter the number: ");
+            string b = Console.ReadLine();
+            int guess = int.Parse(b);
+            while (guess != a)
+            {
+                if (guess > a)
+                {
+                    Console.WriteLine("The number is too high");
+                }
+                else
+                {
+                    Console.WriteLine("The number is too small");
+                }
+                guess = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Congratulations!");
+        }
+    }
+}
